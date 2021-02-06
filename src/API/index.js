4b7b30beaae4d086141f9ed7068a9f6452953ffc
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-02-06 14:25:34 -0300

    Atualizando variáveis de ambiente

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
-console.log("O ENVIROMENT: ", process.env.REACT_APP_BACK)
+console.log("O ENVIROMENT: ", process.env.REACT_APP_ENVIRONMENT)
+
+if (process.env.REACT_APP_ENVIRONMENT == "development"){
+    var url = "http://localhost:3003/"
+} else {
+    var url = "https://automation-back-end.herokuapp.com/" 
+}
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BACK
+    baseURL: url
 })
